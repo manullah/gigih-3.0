ceb3a101ae706fafe3ddd84a653ad96c1b21b4a7
Imanullah <pengguna21@gmail.com>
2023-07-26 11:57:07 +0700

    add | module 4 - init react app

---
+++ module-4/my-react-app/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <section>
+      <div style={{width: '100px', height: '100px'}}></div>
+      <div style={{width: '100px', height: '100px'}}></div>
+    </section>
+      <div style={{display: 'flex', alignItems: 'center', gap: '2rem', border: '1px solid white', padding: '5rem'}}>
+        <img
+          src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
+          alt=""
+          srcSet=""
+          style={{width: '300px', height: '300px', objectFit: 'cover', borderRadius: '50%'}}
+        />
+        <h1>Muhammad Imanullah</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
