802bd0e4dd21f26e8304338a5a3bceeb274127bc
Imanullah <pengguna21@gmail.com>
2023-08-02 19:03:18 +0700

    add | login spotify

--- assessment-midterm/backend/src/models/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  Producut: require("./ProductModel"),
-  Video: require("./VideoModel"),
-  User: require("./UserModel"),
-  Comment: require("./CommentModel"),
-};
--- assessment-midterm/backend/src/utils/config.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-
-const config = {
-  PORT: process.env.PORT || 3000,
-  DATABASE_URL: process.env.DATABASE_URL,
-};
-
-module.exports = config;
